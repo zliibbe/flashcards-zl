e19d70dc3716ce7f8aa74dc68ca1c10b2ecbc7a6
Zach Liibbe <zliibbe@gmail.com>
2022-02-03 12:15:28 -0700

    Add beginning of tests for Round-test and psuedo for Round

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,31 @@
 class Round {
-    constructor() {
-
+    constructor(deck) {
+        this.deck = deck
+        //this.incorrectGuesses = []
+        //store turns in property here
     }
     
+    // takeTurn(strGuess){
+    // increase turn counter
+    // add incorrect answer to incorrect guess array
+    //Turn instantiated e.g. turn = new Turn('sea otter', card1)
+    // turn.evaluateGuess
+    // turn.giveFeedback
+    // }
+
+    // returnCurrentCard()  
+
+    // }
+
+    // calculatePercentCorrect() {
+
+    // }
+
+    // endRound()
+
+    // }
 }
+
+//
+
+module.exports = Round; 
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,11 +2,15 @@ const chai = require('chai');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
-const git  = require('../src/Round');
+const Round = require('../src/Round');
 const expect = chai.expect;
 
-describe('Deck', function() {
-
+describe('Round', function() {
+    let card1;
+    let card2;
+    let deck;
+    let round;
+    let turn;
     beforeEach(() => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
@@ -14,48 +18,71 @@ describe('Deck', function() {
     deck = new Deck([card1, card2, card3]);
 
     round = new Round(deck);
+    //turn = new Turn('sea otter', card1)
     }) 
 
     it('should be a function', function() {
       
-      expect(Deck).to.be.a('function');
+      expect(round).to.be.a('instance');
     });
 
-    it('should return the current card being played', function() {
+    it.skip('should take in an array of cards as a deck to be played with', function() {
+        expect(round.deck).to.eql(deck);
+    })
+
+    it.skip('should return the current card being played', function() {
         round.returnCurrentCard()
 
-        expect(Deck).to.be.a('function');
+        expect(round.returnCurrentCard()).to.equal('card1');
       });
 
-        round.returnCurrentCard()
+    it.skip('should be able to update turn count regardless of whether the guess is correct or incorrect', function() {
+        round.takeTurn();
+        round.takeTurn();
+        round.takeTurn();
 
-    it('should be able to update turn count regardless of if the guess is correct or incorrect', function() {
-      
-      expect(Deck).to.be.a('function');
+        expect(round.turnCounter).to.equal(3);
     });
 
-    it('should have next card become current card', function() {
+    it.skip('should have next card become current card', function() {
+        round.takeTurn();
+
+        expect(round.currentCard).to.equal(card2)//it should have the next index in the deck array become the current card
+    });
+
+    it.skip('should evaluate and record a guess.', function() {
+        
+        expect(round.takeTurn()).to.equal('correct!');
+    });
+
+    it.skip('should have incorrect guesses stored (via the id) in an array of incorrectGuesses', function() {
+        // push incorrect guess to this.incorrectGuesses array
+        round.takeTurn()
+
+        expect(round.incorrectGuesses().length).to.equal(1);
+
+    })
+
+    it.skip('should have feedback returned if the guess is incorrect', function() {
+        round.takeTurn()
+
+        // expect(Deck).to.be.a('function');
+    });
+
+    it.skip('should have feedback returned if the guess is correct', function() {
       
         // expect(Deck).to.be.a('function');
     });
 
-    it('should evaluate and record a guess. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses', function() {
+    it.skip('should have a method that calculates and returns the percentage of correct guesses', function() {
       
-        // expect(Deck).to.be.a('function');
-    });
-
-    it('should have feedback returned regarding whether the guess is incorrect or correct', function() {
-      
-        // expect(Deck).to.be.a('function');
-    });
-
-    it('should have a method that calculates and returns the percentage of correct guesses', function() {
-      
+        //round.calculatePercentCorrect()
         // expect(Deck).to.be.a('function');
     }); 
 
-    it('should have a method that prints that the round has ended', function() {
+    it.skip('should have a method that prints that the round has ended', function() {
         // ‘** Round over! ** You answered <>% of the questions correctly!’
         // expect(Deck).to.be.a('function');
+        expect(endRound()).to.equal('** Round over! ** You answered <>% of the questions correctly!')
     });    
 })
