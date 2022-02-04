d425eccd71dc66e3206717093d56d8f9aff76c15
Zach Liibbe <zliibbe@gmail.com>
2022-02-03 17:44:57 -0700

    Add game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,14 +3,23 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Round = require('../src/Round');
+const Game = require('../src/Game');
 const expect = chai.expect;
 
 describe('Game', function() {
     beforeEach(() => {
-        game = new Game();
-        
+        game = new Game();   
     })
+
     it('should be a function', function() {
-      
-        expect().to.be.a('function');
+      expect(Game).to.be.a('function');
     });
+    
+    it('should keep track of the current round', function() {
+        expect(game.currentRound).to.eql({});
+    });
+    
+    it('should put cards into a deck', function() {
+        expect(new Deck()).to.be.an.instanceof(Deck);
+    });
+})
